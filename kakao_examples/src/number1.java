30dba709792eb24e38facdc75b0637eae39a1aab
rkdls8268 <rkdls8268@gmail.com>
2020-09-12 21:21:53 +0900

    kakao: 2020 하반기 1차 코테

--- kakao_examples/src/number1.java
+++ kakao_examples/src/number1.java
@@ -10,18 +10,65 @@ public class number1 {
 
     // static 처리 안해줬더니 오류 남
     public static int solution(String s) {
-        ArrayList<String> strList = new ArrayList<>();
-        // 문자열 길이는 length() 사용
-        strList.add(s.substring(0));
-        for (int i = 1; i < s.length(); i++) {
-            if (s.substring(i-1) != s.substring(i)) {
-                strList.add(s.substring(i));
-            }
-//            if (s.substring(i, i+1))
+        int answer = Integer.MAX_VALUE;
 
-            System.out.println(strList);
+        for (int len = 1; len <= s.length(); ++len) {
+            String compressed = ""; // 압축된 문자열
+            String target = ""; // 목표 문자열
+            String current = ""; // 현재 문자열
+            int cnt = 1;
+
+            // 첫 번째 단위 지정
+            for (int i = 0; i < len; ++i) {
+                target += s.charAt(i);
+            }
+            System.out.println("target: "+target);
+
+            // 단위만큼 잘랐을 때의 각 부분의 시작 인덱스
+            for (int i = len; i < s.length(); i += len) {
+                current = "";
+                for (int j = i; j < i + len; ++j) {
+                    if (j >= s.length()) break;
+                    current += s.charAt(j);
+                }
+                System.out.println("current: "+current);
+                if (target.equals(current)) {
+                    cnt++;
+                } else {
+                    if (cnt > 1) {
+                        compressed += cnt + target;
+                    } else {
+                        compressed += target;
+                    }
+                    cnt = 1;
+                    target = current;
+                }
+                System.out.println("compressed: "+compressed);
+            }
+
+            if (cnt > 1) {
+                compressed += cnt + target;
+            } else {
+                compressed += target;
+            }
+
+            int length = compressed.length();
+            answer = answer > length ? length : answer;
+            System.out.println("len: "+len);
         }
-        int answer = 0;
+
+//        ArrayList<String> strList = new ArrayList<>();
+//        // 문자열 길이는 length() 사용
+//        strList.add(s.substring(0));
+//        for (int i = 1; i < s.length(); i++) {
+//            if (s.substring(i-1) != s.substring(i)) {
+//                strList.add(s.substring(i));
+//            }
+//            if (s.substring(i, i+1))
+//
+//            System.out.println(strList);
+//        }
+        if (answer == Integer.MAX_VALUE) answer = 1;
         return answer;
     }
 }
