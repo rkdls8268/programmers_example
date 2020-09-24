056a6ebc46973733a7203a2fdef2443149537443
rkdls8268 <rkdls8268@gmail.com>
2020-09-24 15:59:28 +0900

    modify: Hash2

--- hash_examples/src/Hash2.java
+++ hash_examples/src/Hash2.java
@@ -3,7 +3,7 @@ import java.util.HashMap;
 public class Hash2 {
     // 전화번호 목록
     public static void main(String[] args) {
-        String[] phone_book = {"123", "12456", "256"};
+        String[] phone_book = {"123", "123456", "256"};
         System.out.println(solution(phone_book));
     }
 
@@ -12,27 +12,34 @@ public class Hash2 {
 
         HashMap<String, Integer> hm = new HashMap<>();
 
-        // 123 , 123456
-
         for (String pb : phone_book) {
             hm.put(pb, hm.getOrDefault(pb, 0) + 1);
         }
 
         for (String key : hm.keySet()) {
-            System.out.println("key: "+key);
-
             for (String itr : hm.keySet()) {
-                System.out.println(itr);
-                if (key.startsWith(itr)) {
-                    hm.put(key, hm.get(key) - 1);
+                if (!key.equals(itr) && key.startsWith(itr)) {
+                    answer = false;
+                    break;
                 }
             }
-
-            System.out.println(hm.values());
-            if (hm.get(key) < 0) {
-                answer = false;
-            }
         }
+
+//        for (String key : hm.keySet()) {
+//            System.out.println("key: "+key);
+//
+//            for (String itr : hm.keySet()) {
+//                System.out.println(itr);
+//                if (key.startsWith(itr)) {
+//                    hm.put(key, hm.get(key) - 1);
+//                }
+//            }
+//
+//            System.out.println(hm.values());
+//            if (hm.get(key) < 0) {
+//                answer = false;
+//            }
+//        }
 
         return answer;
     }
