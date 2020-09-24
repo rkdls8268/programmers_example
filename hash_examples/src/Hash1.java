c3aadb924fe63f4b8ce96973cddfebff044a3ab7
rkdls8268 <rkdls8268@gmail.com>
2020-09-24 14:51:39 +0900

    modify: hash1

--- hash_examples/src/Hash1.java
+++ hash_examples/src/Hash1.java
@@ -1,4 +1,7 @@
+import javax.sound.midi.SysexMessage;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 public class Hash1 {
     // 완주하지 못한 선수
@@ -6,40 +9,33 @@ public class Hash1 {
     public static void main(String[] args) {
         String[] participant = {"leo", "kiki", "eden", "leo"};
         String[] completion = {"eden", "kiki", "leo"};
-        System.out.println(solution(participant, completion));
+        System.out.println("\nanswer: " + solution(participant, completion));
     }
 
     public static String solution(String[] participant, String[] completion) {
         String answer = "";
-        int val = 0;
 
-        //
         HashMap<String, Integer> hm = new HashMap<>();
-        int i = 0;
 
+        // getOrDefault(): 찾는 키가 존재하면 해당 키의 값을 반환하고 없으면 기본값을 반환함.
+
+        // value 값을 참가자의 수로 지정하고 완주자는 -1 을 해주는 식으로 하면 된다. 마지막에 0이 아닌 참가자 즉, 1 이상이면 그 사람은 완주를 못한 것
         for (String p : participant) {
-            hm.put(p, i++);
-            System.out.println(hm.get(p));
-//            System.out.println(hm.get("eden"));
-
+            hm.put(p, hm.getOrDefault(p, 0) + 1);
+            System.out.println(hm.values()); // 값 출력하는 메서드
         }
+        System.out.println("keyset: "+hm.keySet()); // 키 출력하는 메서드
 
-        // 향상된 for 문: foreach 문
         for (String c : completion) {
-            val = hm.get(c);
-            System.out.println("val: " + val);
-            System.out.println(hm.put(c,val));
+            hm.put(c, hm.get(c) - 1);
+            System.out.println(hm.values());
         }
-
-        // 인덱스를 활용해주자
 
         for (String key : hm.keySet()) {
-            System.out.println(key);
-            if (hm.get(key) == 1) {
+            if (hm.get(key) != 0) {
                 answer = key;
             }
         }
-        System.out.println(hm.keySet());
 
         return answer;
     }
