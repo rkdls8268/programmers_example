d3d08ad657c8fed44275dd9d3347c92f90ba1960
rkdls8268 <rkdls8268@gmail.com>
2020-09-25 22:23:37 +0900

    modify: SQ1 stack 사용

--- stack_queue_examples/src/SQ1.java
+++ stack_queue_examples/src/SQ1.java
@@ -16,6 +16,8 @@ public class SQ1 {
         // 스택으로 처리
 
         Stack<Integer> stack = new Stack<>();
+
+        // 스택에 거꾸로 삽입
         for (int i = prices.length - 1; i >= 0; i--) {
             stack.push(prices[i]);
         }
@@ -24,28 +26,36 @@ public class SQ1 {
 //        }
         System.out.println(stack);
 
-        for (Integer p : prices) {
-
-            stack.push(p);
-//            if (!stack.contains(p)) {
-//                stack.push(p);
-//            } else {
-//                System.out.println("p: "+p+", search: "+stack.search(p));
-//                stack.push(stack.search(p) - 1);
-//            }
-        }
+//        for (Integer p : prices) {
+//
+//            stack.push(p);
+////            if (!stack.contains(p)) {
+////                stack.push(p);
+////            } else {
+////                System.out.println("p: "+p+", search: "+stack.search(p));
+////                stack.push(stack.search(p) - 1);
+////            }
+//        }
 
         int cnt = 0;
 
         for (int i = 0; i < len; i++) {
-            System.out.println(stack.peek());
-            if (!stack.contains(stack.peek())) {
-                answer[i] = cnt;
-                stack.pop();
-            } else {
-                answer[i] = stack.search(stack.peek());
-                stack.pop();
+            int cur = stack.pop();
+            System.out.println("cur:" + cur);
+            for (int j = i + 1; j < len; j++) {
+                if (cur <= prices[j]) {
+                    cnt++;
+                } else if (cur > prices[j]){
+//                    System.out.println(stack.peek());
+//                    answer[i] = stack.search(stack.peek());
+//                    stack.pop();
+                    cnt++;
+                    break;
+                }
+
             }
+            answer[i] = cnt;
+            cnt = 0;
         }
 
         return answer;
