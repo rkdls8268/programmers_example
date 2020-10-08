f5c5403ff6bb9c23f4d5c1333cba88ea14357ef9
rkdls8268 <rkdls8268@gmail.com>
2020-10-08 17:56:17 +0900

    modify: Heap1

--- heap_example/src/Heap1.java
+++ heap_example/src/Heap1.java
@@ -1,6 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 public class Heap1 {
     public static void main(String[] args) {
@@ -14,33 +12,23 @@ public class Heap1 {
         // 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + 두 번째로 맵지 않은 음식의 스코빌 지수 * 2
         int answer = 0;
 
-        int len = scoville.length;
-
-        ArrayList<Integer> heap = new ArrayList<>();
+        Queue<Integer> pq = new PriorityQueue<>();
         for (Integer s : scoville) {
-            heap.add(s);
+            pq.add(s);
         }
-        System.out.println(heap);
 
         int temp = 0;
-        for (int i = 0; i < heap.size(); i++) {
-            System.out.println("i: "+i);
-            if (heap.get(i) < K) {
-                temp = heap.get(i) + (heap.get(i + 1) * 2);
-                System.out.println("i: "+heap.get(i) + ", i+1: "+heap.get(i+1));
-//                heap.remove(i);
-                System.out.println(heap.remove(i+1));
-                System.out.println(heap.remove(i));
-//                heap.remove(i+1);
-                heap.add(0, temp);
-                System.out.println(heap);
-                answer++;
-                i=-1;
-            }
-            Collections.sort(heap);
-        }
+        for (int i = 0; pq.peek() < K; i++) {
+//            System.out.println(pq);
+            if (pq.size() == 1) return -1;
 
-        if (answer == len) answer = -1;
+            int first = pq.poll();
+            int second = pq.poll();
+            temp = first + (second * 2);
+            pq.add(temp);
+            answer++;
+
+        }
         return answer;
     }
 
