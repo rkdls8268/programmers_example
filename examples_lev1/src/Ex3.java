4c4bd6693ed84d45bc5ad1375537b6061b2eed8b
rkdls8268 <rkdls8268@gmail.com>
2020-10-26 23:15:33 +0900

    complete: Ex3

--- examples_lev1/src/Ex3.java
+++ examples_lev1/src/Ex3.java
@@ -4,21 +4,34 @@ public class Ex3 {
     // 문자열 내 마음대로 정렬하기
     public static void main(String[] args) {
         String[] strings = {
-                "sun", "bed", "car"
+                "abce", "abcd", "cdx"
         };
-        int n = 1;
+        int n = 2;
         System.out.println(Arrays.toString(solution(strings, n)));
     }
 
     static String[] solution(String[] strings, int n) {
-        String[] answer = {};
-        answer = new String[strings.length];
-        Queue<String> queue = new LinkedList<>();
-        List<Character> list = new ArrayList<>();
-        for (int i = 0; i < strings.length; i++) {
-            list.add(strings[i].charAt(n));
-        }
+//        String[] answer = {};
+//        answer = new String[strings.length];
+        // 오름차순 정렬 먼저 해줌
+        Arrays.sort(strings);
+        // comparator 사용
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                Character c1 = o1.charAt(n);
+                Character c2 = o2.charAt(n);
+                return c1 > c2 ? 1 : (c1 == c2 ? 0 : -1);
+            }
+        });
 
-        return answer;
+
+//        for (int i = 0; i < strings.length; i++) {
+//            answer[i] = strings[i];
+//        }
+
+        return strings;
     }
 }
+
+// list에 인덱스 값을 add하는 식으로 구현했는데 이렇게 하지말고 i 값을 저장하는 식으로 하면 안되나..?
