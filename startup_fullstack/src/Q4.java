e35a37f75448914b32e112c44bfcc0dbade6ebc3
rkdls8268 <rkdls8268@gmail.com>
2020-11-04 17:02:07 +0900

    Q4

--- startup_fullstack/src/Q4.java
+++ startup_fullstack/src/Q4.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Q4 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+public class Q4 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        int[][] arr = new int[4][5];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                int n = random.nextInt(40)+1;
+                if (list.contains(n)) {
+//                    System.out.println("n1: "+n);
+                    do {
+                        n = random.nextInt(40)+1;
+                    } while (!list.contains(n));
+                }
+                list.add(n);
+//                System.out.println(list);
+                arr[i][j] = n;
+            }
+        }
+
+        System.out.println(Arrays.deepToString(arr));
+    }
+
+    static int[] solution(int[] numbers) {
+        int[] answer = {};
+        return answer;
+    }
 }
