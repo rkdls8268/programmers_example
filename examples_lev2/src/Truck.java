7f97a90db7f58276c3138c0175b77233c53ab694
rkdls8268 <rkdls8268@gmail.com>
2020-12-05 20:47:00 +0900

    [연습] 다리를 지나는 트럭

--- examples_lev2/src/Truck.java
+++ examples_lev2/src/Truck.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
@@ -13,21 +14,34 @@ public class Truck {
 
     static int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0; // 걸린 최소 시간
-
         Queue<Integer> queue = new LinkedList<>();
+        // queue 의 길이가 bridge_length 만 해야 함 (queue.size() == bridge_length)
+        int sum = 0;
         for (int t : truck_weights) {
-            queue.add(t);
+            while (true) {
+                // queue가 비어있는 경우
+                if (queue.isEmpty()) {
+                    queue.add(t);
+                    sum += t;
+                    answer++;
+                    break;
+                } else if (queue.size() == bridge_length) {
+                    // queue 가 가득 차 있는 경우
+                    sum -= queue.poll();
+                } else {
+                    // 트럭이 다리 위에 있지만 queue 가 가득 차 있지 않은 상태
+                    if (sum + t > weight) {
+                        queue.add(0); // 0을 넣어주는 게 포인트!!! * 어차피 더해도 값에 변화 없으니까
+                        answer++;
+                    } else {
+                        queue.add(t);
+                        sum += t;
+                        answer++;
+                        break;
+                    }
+                }
+            }
         }
-        System.out.println(queue);
-        Stack<Integer> stack = new Stack<>();
-        // TODO: stack에 차례대로 넣고 만약 무게 < weight 이면 건너가게 아니면 빼고
-
-        int len = truck_weights.length;
-        int first = 0;
-        while (!queue.isEmpty()) {
-            if (stack.isEmpty()) stack.push(truck_weights[first]);
-//            for (stack.peek())
-        }
-        return answer;
+        return answer + bridge_length;
     }
 }
